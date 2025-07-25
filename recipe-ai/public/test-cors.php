d7166a6315eccb14f4eb23e4a89806e8141c25ae
Jesus Gutierrez <jg277893@my.stchas.edu>
2025-07-25 18:05:56 -0500

    Added styles, organized files

--- recipe-ai/public/test-cors.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
-
-echo json_encode(["test" => "CORS working"]);
-?>
--- scrap/smart-grocery-react-test/start-app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\Owner\Documents\GitHub\MealMind\scrap\smart-grocery-react-test
-npm start
-pause
